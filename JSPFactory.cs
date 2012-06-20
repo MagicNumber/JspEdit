4e2dce717fef0b70189536ae287f06cb7a08505a
MagicNumber <madeggs@googlemail.com>
2012-06-20 19:28:10 +0100

    Removing redundant code

--- JSPFactory.cs
+++ JSPFactory.cs
@@ -44,90 +44,80 @@ namespace JspEdit
         public static JSP Load( BinaryReader stream )
         {
             int pos = 0;
-            try
+
+            Int16 ImageCount = stream.ReadInt16();
+            pos += 2;
+            if ( ImageCount < 1 )
+                throw new InvalidDataException( "Non-positive image count" );
+
+            int[] dataLengths = new int[ImageCount];
+
+            JSP Collection = new JSP();
+
+            for ( int im = 0; im < ImageCount; im++ )
             {
-                Int16 ImageCount = stream.ReadInt16();
-                pos += 2;
-                if ( ImageCount < 1 )
-                    throw new InvalidDataException( "Non-positive image count" );
+                Int16 width = stream.ReadInt16();
+                Int16 height = stream.ReadInt16();
+                pos += 2 + 2;
+                if ( width < 1 || height < 1 )
+                    throw new InvalidDataException( string.Format( "Image {0} has a height/width of {1},{2}. This doesn't make sense.", im, height, width ) );
 
-                int[] dataLengths = new int[ImageCount];
+                Int16 ofsX = stream.ReadInt16();
+                Int16 ofsY = stream.ReadInt16();
+                pos += 2 + 2;
 
-                JSP Collection = new JSP();
+                Int32 dataSize = stream.ReadInt32();
+                pos += 4;
 
-                for ( int im = 0; im < ImageCount; im++ )
+                stream.ReadInt32(); // Four blank bytes.
+                pos += 4;
+
+                dataLengths[im] = dataSize;
+
+                JSPImage Image = new JSPImage( width, height );
+                Image.SetOffSet( ofsX, ofsY );
+                Collection.Images.Add( Image );
+
+            }
+
+            for ( int ImageNum = 0; ImageNum < ImageCount; ImageNum++ )
+            {
+                List<byte> imgdata = new List<byte>();
+
+                for ( int byteIndex = 0; byteIndex < dataLengths[ImageNum]; byteIndex++ )
                 {
-                    Int16 width = stream.ReadInt16();
-                    Int16 height = stream.ReadInt16();
-                    pos += 2 + 2;
-                    if ( width < 1 || height < 1 )
-                        throw new InvalidDataException( string.Format( "Image {0} has a height/width of {1},{2}. This doesn't make sense.", im, height, width ) );
+                    byte sb = stream.ReadByte();
+                    pos += 1;
+                    bool negative = sb > 128;
+                    byte usb = (byte) ( sb & 0x7F );
 
-                    Int16 ofsX = stream.ReadInt16();
-                    Int16 ofsY = stream.ReadInt16();
-                    pos += 2 + 2;
-
-                    Int32 dataSize = stream.ReadInt32();
-                    pos += 4;
-
-                    stream.ReadInt32(); // Four blank bytes.
-                    pos += 4;
-
-                    dataLengths[im] = dataSize;
-
-                    JSPImage Image = new JSPImage( width, height );
-                    Image.SetOffSet( ofsX, ofsY );
-                    Collection.Images.Add( Image );
-
-                }
-
-                for ( int ImageNum = 0; ImageNum < ImageCount; ImageNum++ )
-                {
-                    List<byte> imgdata = new List<byte>();
-
-                    for ( int byteIndex = 0; byteIndex < dataLengths[ImageNum]; byteIndex++ )
+                    if ( negative )
                     {
-                        byte sb = stream.ReadByte();
-                        pos += 1;
-                        bool negative = sb > 128;
-                        byte usb = (byte) ( sb & 0x7F );
-
-                        if ( negative )
+                        for ( int i = 0; i < usb; i++ )
                         {
-                            for ( int i = 0; i < usb; i++ )
-                            {
-                                imgdata.Add( 0 );
-                            }
-                        }
-                        else
-                        {
-                            for ( int i = 0; i < usb; i++ )
-                            {
-                                imgdata.Add( stream.ReadByte() );
-                                pos += 1;
-                                byteIndex++;
-                            }
+                            imgdata.Add( 0 );
                         }
                     }
-
-                    Collection.Images[ImageNum].SetData( imgdata.ToArray() );
+                    else
+                    {
+                        for ( int i = 0; i < usb; i++ )
+                        {
+                            imgdata.Add( stream.ReadByte() );
+                            pos += 1;
+                            byteIndex++;
+                        }
+                    }
                 }
 
+                Collection.Images[ImageNum].SetData( imgdata.ToArray() );
+            }
 
-                return Collection;
-            }
-            /*catch ( EndOfStreamException EoS )
-            {
-                EoS.Data["position"] = pos;
-                throw;
-            }*/
-            finally
-            {
-            }
+            return Collection;
         }
 
         public static void Save( JSP obj, Stream sout )
         {
+
             return;
         }
 
